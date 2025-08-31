c4b27b8a5399f75680c2c3428b3111da2fd3337f
Dhiman Dasgupta <dasguptadhiman5@gmail.com>
2025-08-31 15:27:24 +0530

    Update ci.yml

--- src/main/java/base/DriverFactory.java
+++ src/main/java/base/DriverFactory.java
@@ -5,7 +5,7 @@ import com.codeborne.selenide.Configuration;
 public class DriverFactory {
     public static void setupDriver() {
         Configuration.browser = "chrome";
-        Configuration.headless = false;
+        Configuration.headless = true;
         Configuration.browserSize = "1920x1080";
         Configuration.timeout = 8000;
     }
