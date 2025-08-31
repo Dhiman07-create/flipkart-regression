79908ba946c8be6b6648b67579f5356134fa9e9d
Dhiman Dasgupta <dasguptadhiman5@gmail.com>
2025-08-31 22:35:56 +0530

    Update ci.yml

--- src/test/java/tests/RegressionTest.java
+++ src/test/java/tests/RegressionTest.java
@@ -1,12 +1,15 @@
 package tests;
 
 import base.BaseTest;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
 import org.testng.annotations.Test;
 import pages.HomePage;
 import pages.SearchResults;
 
 import static com.codeborne.selenide.Selenide.open;
-
+@Epic("Flipkart Tests")
+@Feature("Regression")
 public class RegressionTest extends BaseTest {
     @Test(description = "Verify product search")
     public void testSearchProduct() {
